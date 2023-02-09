ae5918ed3a958f875d840099fdec4adcb5dc1d5e
Rossana Suarez <46635001+roxsross@users.noreply.github.com>
2023-02-08 22:02:35 -0300

    Update and rename version.sh to telegram.sh

---
+++ telegram.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+BOT_URL=$URL
+TELEGRAM_CHAT_ID="-1001508340482"
+# Set formatting
+PARSE_MODE="Markdown"
+COMMIT=$(git log -1 --pretty=format:"%s")
+BUILD_STATUS="SUCCESS"
+REPOSITORY="jenkins-demo"
+BRANCH_NAME="master"
+VERSION="1.0.0"
+
+# Send message function
+send_msg () {
+    curl -s -X POST ${BOT_URL} -d chat_id=$TELEGRAM_CHAT_ID \
+        -d text="$1" -d parse_mode=${PARSE_MODE}
+}
+
+
+# Call send message with the message
+send_msg "
+\`-----------------------------------------------------------\`
+Deploy 🚀*${BUILD_STATUS}!*
+\`Repository 📦:  ${REPOSITORY}\`
+\`Branch 🏷:      ${BRANCH_NAME}\`
+\`Version ✅:      ${VERSION}\`
+*Commit Msg 💭:*
+_${COMMIT}_
+[Job Log view here 🤟](${BUILD_URL})
+\`-----------------------------------------------------------\`
+"
+
